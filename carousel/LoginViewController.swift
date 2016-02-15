9ca6e6e1e23109cd8f434ad030b92efefc14a01e
Jason Putorti <jason@brigade.com>
2016-02-14 17:19:49 -0800

    Setting up login view
    Known issue, now app is getting stuck on initial nav controller

--- carousel/LoginViewController.swift
+++ carousel/LoginViewController.swift
@@ -8,10 +8,26 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var signInScroller: UIScrollView!
+    @IBOutlet weak var fieldParentView: UIView!
+    @IBOutlet weak var buttonParentView: UIView!
+    var buttonInitialY: CGFloat!
+    var buttonOffset: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        signInScroller.delegate = self
+        signInScroller.contentSize = signInScroller.frame.size
+        signInScroller.contentInset.bottom = 100
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        buttonInitialY = buttonParentView.frame.origin.y
+        buttonOffset = -120
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +37,22 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func goBack(sender: AnyObject) {
+        navigationController!.popViewControllerAnimated(true)
+    }
+    
+    func keyboardWillShow(notification: NSNotification!) {
+        print("keyboardWillShow")
+        // Move the button up above keyboard
+        buttonParentView.frame.origin.y = buttonInitialY + buttonOffset
+        // Scroll the scrollview up
+        signInScroller.contentOffset.y = signInScroller.contentInset.bottom
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        // Move the buttons back down to it's original position
+        buttonParentView.frame.origin.y = buttonInitialY
+    }
 
     /*
     // MARK: - Navigation
